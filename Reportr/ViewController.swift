98fa224707d252f4a6eda36dc72beefdca06390d
Pranav Teegavarapu <pranavnt@outlook.com>
2020-06-06 23:51:20 -0700

    commiting work so far

--- Reportr/ViewController.swift
+++ Reportr/ViewController.swift
@@ -6,15 +6,73 @@
 //  Copyright © 2020 Pranav Teegavarapu. All rights reserved.
 //
 
+
 import UIKit
+import MobileCoreServices
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+    var controller = UIImagePickerController()
+    let videoFileName = "/video.mp4"
+    
+    @IBAction func takeVideo(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            controller.sourceType = .camera
+            controller.mediaTypes = [kUTTypeMovie as String]
+            controller.delegate = self
+            
+            present(controller, animated: true, completion: nil)
+        }
+        else {
+            print("Camera is not available")
+        }
+    }
+    
+    @IBAction func viewLibrary(_ sender: Any) {
+        controller.sourceType = UIImagePickerController.SourceType.photoLibrary
+        controller.mediaTypes = [kUTTypeMovie as String]
+        controller.delegate = self
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func sendMail(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedVideo:URL = (info[UIImagePickerController.InfoKey.mediaURL] as? URL) {
+            let selectorToCall = #selector(ViewController.videoSaved(_:didFinishSavingWithError:context:))
+            UISaveVideoAtPathToSavedPhotosAlbum(selectedVideo.relativePath, self, selectorToCall, nil)
+            
+            let videoData = try? Data(contentsOf: selectedVideo)
+            let paths = NSSearchPathForDirectoriesInDomains(
+                FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+            let documentsDirectory: URL = URL(fileURLWithPath: paths[0])
+            let dataPath = documentsDirectory.appendingPathComponent(videoFileName)
+            try! videoData?.write(to: dataPath, options: [])
+        }
+        picker.dismiss(animated: true)
+    }
+    
+    @objc func videoSaved(_ video: String, didFinishSavingWithError error: NSError!, context: UnsafeMutableRawPointer){
+        if let theError = error {
+            print("error saving the video = \(theError)")
+        } else {
+            DispatchQueue.main.async(execute: { () -> Void in
+            })
+        }
+    }
 }
+
 
